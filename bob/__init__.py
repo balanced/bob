a785816d83dd06e3130e5576e65b53ecddb42357
Marshall Jones <marshall@balancedpayments.com>
2014-08-21 10:14:14 -0700

    add version

--- bob/__init__.py
+++ bob/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 class BobError(Exception):
--- bob/commands/__init__.py
+++ bob/commands/__init__.py
@@ -1,8 +1,17 @@
 from __future__ import unicode_literals
 
+import click
+
+import bob
 from . import build, serve
 
 
+@click.command('version')
+def version():
+    click.echo(bob.__version__)
+
+
 def add_commands(group):
+    group.add_command(version)
     group.add_command(build.group)
     group.add_command(serve.serve)
